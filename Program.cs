1a45c7225f7729d9b0519f3cf3b9252570179293
Fábio Lira <fabiolira1910@hotmail.com>
2019-08-08 18:25:35 -0300

    trainComposition
    commit de tudo

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace trainComposition
+{
+	class Train
+	{
+		public int count = 0;
+		
+		public struct Wagon{
+			public int hierarchyLevel;
+		}
+		
+		public Wagon newWagon(int hierarchy){
+			Wagon wagon = new Wagon();
+			wagon.hierarchyLevel = hierarchy;
+			count++;
+			return wagon;
+		}
+		
+		public Queue newTrain(){
+			Queue train =  new Queue();
+			return train;
+		}
+		
+		public void queueWagon(int hierarchy, Queue train){
+			train.Enqueue(newWagon(hierarchy));
+		}
+		
+		public int dequeueWagon(Queue train){
+			Wagon wagon = new Wagon();
+			wagon = (Wagon) train.Dequeue();
+			return wagon.hierarchyLevel;
+		}
+	}
+	
+	class Program{
+		public static void Main(string[] args)
+		{	
+			Train tr = new Train();
+			var train = tr.newTrain();
+			tr.queueWagon(7, train);
+			tr.queueWagon(10, train);
+			for(int i = 0; i < tr.count; i++){
+				Console.WriteLine("{0}º vagão a sair: {1}; ", i+1, tr.dequeueWagon(train));
+			}
+			Console.ReadKey(true);
+		}
+	}
+}
